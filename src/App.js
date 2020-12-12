a86546a496c3f97d2fd44e75ec9a58f00d1f8f34
bishank <chandbishank@gmail.com>
2020-12-11 19:14:15 -0700

    new page created and route setup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
-const allCategories = ["all", ...new Set(items.map((item) => item.category))]
+import React from 'react';
+import Homepage from "./pages/Home";
+import { BrowserRouter, Route } from "react-router-dom";
+
+
+
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
-  const filterItems = (category) => {
-    if (category === 'all') {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
   return (
-    <main>
-      <section className="menu-section">
-        <div className="title">
-          <h2>Tibet Kitchen</h2>
-          <div className="underline"></div>
-        </div>
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </section>
-    </main>
+    <BrowserRouter>
+      <Navbar />
+      <div>
+        <Route path="/" component={Homepage} />
+        
+      </div>
+      <Footer />
+    </BrowserRouter>
 
   );
 }
